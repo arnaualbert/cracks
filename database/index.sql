b15c51e73044ae6fdd37f1a902f09c559400fd39
arnaualbert <arnaualbert2003@gmail.com>
2024-04-03 21:27:06 +0200

    start project

---
+++ database/index.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    username VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(100),
+    surname VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS user_services (
+    service_id INT PRIMARY KEY,
+    service_name VARCHAR(100),
+    username VARCHAR(50),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
---
+++ webbapp/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+from controllers.login_controller import check_is_logged
+from controllers.login_controller import login_module
+
+app = Flask(__name__)
+
+app.register_blueprint(login_module)
+
+
+@app.route("/",methods=["GET","POST"])
+def home_page():
+    if request.method == "GET":
+        return render_template("index.html")
+
+
+@app.route("/home_user")
+def home_user():
+    if request.method == "GET" and check_is_logged():
+        return render_template("home_user.html")
+    else:
+        return "please log in"
+    
+@app.route("/logout")
+def logout():
+    if request.method == "GET":
+        return redirect("/")
+    
