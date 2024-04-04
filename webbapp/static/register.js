9992fc11d208669ec9e18bd20e4f32a5ad48c7bb
arnaualbert <arnaualbert2003@gmail.com>
2024-04-04 16:25:53 +0200

    documentar js

--- webbapp/static/register.js
+++ webbapp/static/register.js
@@ -1,3 +1,5 @@
+
+// Función para verificar si dos contraseñas son iguales
 function checkBothPasswords(passwordOne,passwordTwo){
     if (passwordOne === passwordTwo){
         return true
@@ -6,18 +8,58 @@ function checkBothPasswords(passwordOne,passwordTwo){
     }
 }
 
+// Función para verificar si todas las variables no están vacías
+function notEmptyVars(passwordOne,passwordTwo,username,email,name,surname){
 
+    var passwordOneCheck = passwordOne.trim()
+    var passwordTwoCheck = passwordTwo.trim()
+    var usernameCheck = username.trim()
+    var emailCheck = email.trim()
+    var nameCheck = name.trim()
+    var surnameCheck = surname.trim()
+
+    if (passwordOneCheck === '' || 
+        passwordTwoCheck === '' || 
+        usernameCheck === '' || 
+        emailCheck === '' || 
+        nameCheck === '' || 
+        surnameCheck === '') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Función para habilitar o deshabilitar el botón de registro según la validez de los datos del formulario
 function ableDisableButton(){
+
+    // get variables from form
     var passwordOne = document.getElementById("password").value
     var passwordTwo = document.getElementById("passwordrepeat").value
+    var username = document.getElementById("username").value
+    var email = document.getElementById("email").value
+    var name = document.getElementById("name").value
+    var surname = document.getElementById("surname").value
+
+    // messages and button
     var message = document.getElementById("message_passwords")
     var button = document.getElementById("button_register")
 
-    if (checkBothPasswords(passwordOne,passwordTwo)){
-        button.disabled = false
-        message.innerHTML = "" 
+    // checks
+    var notEmptyVarsCheck = notEmptyVars(passwordOne,passwordTwo,username,email,name,surname)
+    var bothPasswordsEqual = checkBothPasswords(passwordOne,passwordTwo)
+
+    if (notEmptyVarsCheck){
+        if (bothPasswordsEqual){
+            button.disabled = false
+            message.innerHTML = "" 
+        }else{
+            button.disabled = true
+            message.innerHTML = "Both passwords should match."
+        }
     }else{
         button.disabled = true
-        message.innerHTML = "Both passwords should match"
+        message.innerHTML = "All fields should be filled."
     }
+
 }
