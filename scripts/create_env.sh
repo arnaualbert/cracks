934d0643409477e116093d2e6ad9db5a340f3d65
arnaualbert <arnaualbert@gmail.com>
2024-04-09 19:45:19 +0200

    create env sh

---
+++ scripts/create_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install mariadb-server
+sudo apt-get install libmariadb-dev
+sudo apt install python3-pip
+sudo apt install python3.10-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install Flask
+pip install python-dotenv
+pip install mariadb
