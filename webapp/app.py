ac49864404bbc808ce68eeda038a1bfa4990879c
arnaualbert <arnaualbert@gmail.com>
2024-04-15 20:08:07 +0200

    own env os function

---
+++ webapp/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from controllers.login_controller import check_is_logged
+from controllers.login_controller import login_module
+from controllers.databases_controller import databases_module
+from controllers.kvm_controller import kvm_module
+from controllers.services_controller import services_module
+from controllers.storage_controller import storage_module
+
+
+app = Flask(__name__)
+
+app.register_blueprint(login_module)
+app.register_blueprint(databases_module)
+app.register_blueprint(kvm_module)
+app.register_blueprint(services_module)
+app.register_blueprint(storage_module)
+
+@app.route("/",methods=["GET","POST"])
+def home_page():
+    if request.method == "GET":
+        return render_template("index.html")
+
+
+@app.route("/home_user")
+def home_user():
+    if request.method == "GET" and check_is_logged():
+        return render_template("home_user.html")
+    else:
+        return "please log in"
+
+
+@app.route("/logout")
+def logout():
+    if request.method == "GET":
+        return redirect("/")
+    
---
+++ webapp/own_env.py
@@ -0,0 +1,14 @@
+import pathlib
+
+def create_dict_env():
+    file_env = pathlib.Path("env").read_text().split()
+    dict_env = {}
+    for kv in file_env:
+        list_kv = kv.split("=")
+        dict_env[list_kv[0]] = list_kv[1]
+    return dict_env
+
+
+def getenv(key):
+    dict_find = create_dict_env()
+    return dict_find[key]
