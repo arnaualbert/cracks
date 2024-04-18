edaeb99e8201e2bc12681e01cc5b94eff66c7d1c
medbenhammou <benhammou1012@gmail.com>
2024-04-18 16:00:37 +0200

    cambios

--- webapp/venv/bin/activate.fish
+++ webapp/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alumne/Desktop/cracks/webapp/venv"
+set -gx VIRTUAL_ENV "/home/mohamed/Desktop/cracks/webapp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
