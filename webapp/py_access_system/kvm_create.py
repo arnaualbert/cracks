f1309ec512bc455db4fcfb5eed157d7b0b024530
arnaualbert <arnaualbert2003@gmail.com>
2024-04-22 20:58:07 +0200

    create kvm

--- webapp/py_access_system/kvm_create.py
+++ webapp/py_access_system/kvm_create.py
@@ -2,26 +2,26 @@ import subprocess
 from controllers.login_controller import get_connection
 from flask import Flask, render_template, request, redirect, session
 
-def create_vm(vm_name,ram,cpus):
-    
+def create_vm(kvm_name,kvm_memory,kvm_cpus):
 
-    command = f"""tsp virt-install --name {vm_name} --memory {ram} --vcpus {cpus} --disk size=15 --cdrom /var/lib/libvirt/images/bionicpup64-8.0-uefi.iso --import --network network:hostbridge --vnc --os-variant unknown"""
 
+    command = f"""tsp virt-install --name {kvm_name} --memory {kvm_memory} --vcpus {kvm_cpus} --disk size=15 --cdrom /var/lib/libvirt/images/bionicpup64-8.0-uefi.iso --import --network network:hostbridge --vnc --os-variant unknown"""
+    kvm_iso = "bionicpup64-8.0-uefi.iso"
     try:
         subprocess.call(command,shell=True)
-        print(f"VM {vm_name} created successfully.")
+        print(f"VM {kvm_name} created successfully.")
         username = session.get("username")
         # Insertar datos en la tabla user_kvm
-        insert_kvm(username,vm_name)
+        insert_kvm(kvm_name, kvm_memory, kvm_cpus, kvm_iso, username)
         print("ASsadsad")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
 
 
-def insert_kvm(username,vm_name):
+def insert_kvm(kvm_name, kvm_memory, kvm_cpus, kvm_iso, username):
     conn = get_connection()
     cur = conn.cursor()
     print("asdsds")
-    cur.execute("INSERT INTO user_kvm (kvm_name, username) VALUES (?, ?)",(vm_name,username))
+    cur.execute("INSERT INTO user_kvm (kvm_name, kvm_memory, kvm_cpus, kvm_iso, username) VALUES (?, ?, ?, ?, ?)",(kvm_name, kvm_memory, kvm_cpus, kvm_iso, username))
     conn.commit()
     conn.close()
