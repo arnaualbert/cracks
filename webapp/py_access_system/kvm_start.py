10a77a37ff126719ae839b746fff4f034ae29275
medbenhammou <benhammou1012@gmail.com>
2024-04-26 17:48:41 +0200

    “start_kvm”

---
+++ webapp/py_access_system/kvm_start.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def start_vm(kvm_name):
+    try:
+        command_start = f"""virsh start {kvm_name}""" 
+        subprocess.call(command_start, shell=True)
+        print(f"VM {kvm_name} Started successfully.")
+
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
