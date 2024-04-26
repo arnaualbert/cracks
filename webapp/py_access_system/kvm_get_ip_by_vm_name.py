d185f057a6a31cdcda68419b1092d3f6ad4254f5
arnaualbert <arnaualbert2003@gmail.com>
2024-04-26 16:33:34 +0200

    comments on get ip from kvm

--- webapp/py_access_system/kvm_get_ip_by_vm_name.py
+++ webapp/py_access_system/kvm_get_ip_by_vm_name.py
@@ -2,7 +2,15 @@ import subprocess
 import re
 import xml.etree.ElementTree as ET
 
-def get_br0_ip():
+def get_br0_ip() -> str|None:
+    """
+    Obtiene la dirección IP y la máscara de subred de la interfaz de red 'br0'.
+    Calcula la máscara de subred en formato CIDR y devuelve la dirección IP en ese formato,
+    o None si no se puede obtener la información.
+    
+    Returns:
+        str|None: Dirección IP en formato CIDR o None si la información no está disponible.
+    """
     ip_add = subprocess.check_output(["ifconfig","br0"])
     ip_match = re.search(r'inet (\d+\.\d+\.\d+\.\d+)', ip_add.decode())
     mask_match = re.search(r'mask (\d+\.\d+\.\d+\.\d+)', ip_add.decode())
@@ -14,11 +22,29 @@ def get_br0_ip():
     else:
         return None
 
-def get_xml(vm_name):
+def get_xml(vm_name: str) -> str:
+    """
+    Obtiene el XML que describe la configuración de la máquina virtual especificada.
+    
+    Args:
+        vm_name (str): Nombre de la máquina virtual.
+    
+    Returns:
+        str: XML que describe la configuración de la máquina virtual.
+    """
     output_xml = subprocess.check_output(["virsh", f"dumpxml --domain {vm_name}"])
     return output_xml.decode()
 
-def get_mac(vm_name):
+def get_mac(vm_name: str) -> str:
+    """
+    Obtiene la dirección MAC de la interfaz de red de la máquina virtual especificada.
+    
+    Args:
+        vm_name (str): Nombre de la máquina virtual.
+    
+    Returns:
+        str: Dirección MAC de la interfaz de red.
+    """
     xml_parse = get_xml(vm_name)
     et_parser = ET.fromstring(xml_parse)
     interface = et_parser.find(".//interface")
@@ -26,10 +52,23 @@ def get_mac(vm_name):
     return mac_address
 
 def do_nmap_analysis():
+    """
+    Realiza un escaneo de hosts en la red br0 utilizando el comando nmap con la opción -sP.
+    La salida se redirige a /dev/null para que no se muestre en la consola.
+    """
     ip_to_parse = get_br0_ip()
     subprocess.run(["nmap","-sP",ip_to_parse],stdout = subprocess.DEVNULL)
 
-def get_ip_kvm(vm_name):
+def get_ip_kvm(vm_name: str) -> str|any|None:
+    """
+    Obtiene la dirección IP de la máquina virtual especificada en la red.
+    
+    Args:
+        vm_name (str): Nombre de la máquina virtual.
+    
+    Returns:
+        str|any|None: Dirección IP de la máquina virtual, None si no se puede obtener.
+    """
     do_nmap_analysis()
     mac_add = get_mac(vm_name)
     out_arp = subprocess.check_output(["arp","-a"])
@@ -43,5 +82,3 @@ def get_ip_kvm(vm_name):
                 return match.group(1)
             else:
                 return None
-
-print(get_ip_kvm("1605master"))
