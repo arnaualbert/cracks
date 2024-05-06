166a8059eb3e8416347b370b026473cb77c18890
arnaualbert <arnaualbert2003@gmail.com>
2024-05-06 20:28:51 +0200

    databases

--- webapp/controllers/databases_controller.py
+++ webapp/controllers/databases_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, redirect, make_response
 from controllers.login_controller import check_is_logged
+from py_access_system.database_create import create_database
 
 databases_module = Blueprint('databases_module', __name__, template_folder='templates')
 
@@ -20,9 +21,14 @@ def user_database_create():
         return redirect("/login")
     
 
-@databases_module.route("/create_database")
+@databases_module.route("/create_database", methods=["GET","POST"])
 def user_create_database():
-    if request.method == "GET" and check_is_logged():
+    if request.method == "POST" and check_is_logged():
+        database_type = request.form["dbType"]
+        database_name = request.form["dbName"]
+        database_user = request.form["dbAdminUser"]
+        database_password = request.form["dbAdminPassword"]
+        create_database(database_type,database_password,database_name,database_user)
         return redirect("/database")
     else:
         return redirect("/login")
--- webapp/py_access_system/database_create.py
+++ webapp/py_access_system/database_create.py
@@ -1,11 +1,30 @@
 import os
 import subprocess
 
+import pathlib
+
+def create_dict_env():
+    file_env = pathlib.Path(".env").read_text().split()
+    dict_env = {}
+    for kv in file_env:
+        list_kv = kv.split("=")
+        dict_env[list_kv[0]] = list_kv[1]
+    return dict_env
+
+
+def getenv(key):
+    dict_find = create_dict_env()
+    return dict_find[key]
+
 def create_database(database_type,database_password,database_name,database_user):
     if database_type == "mariadb" or database_type == "mysql":
+        original_directory = os.getcwd()
+        passw = getenv("PASSWORD_ROOT")
         create_mdb_mysql(database_type,database_password,database_name,database_user)
-        subprocess.call("ls",cwd=f"/home/arnau/Desktop/{database_name}")
-        subprocess.call("cat docker-compose.yml",cwd=f"/home/arnau/Desktop/{database_name}")
+        os.chdir(f"/home/arnau/Desktop/{database_name}")
+        command = f"""echo '{passw}' | sudo -S docker compose up -d"""
+        subprocess.run(command, shell=True, check=True) 
+        os.chdir(original_directory)
     elif database_type == "postgress":
         pass
 
@@ -38,22 +57,22 @@ def create_mdb_mysql(database_type,database_password,database_name,database_user
 
 
 
-create_database("mariadb","a123","arnau","mohamed")
+# create_database("mariadb","a1223","arnsau","mohamsed")
 
 
-def create_postgress(database_name,database_user,database_password):
-    compose = f"""
-services:
-  postgresql:
-    image: postgres
-    restart: always
-    environment:
-      POSTGRES_DB: {database_name}
-      POSTGRES_USER: {database_user}
-      POSTGRES_PASSWORD: {database_password}
-    ports:
-      - "5472:5432"
-    volumes:
-      - /home/arnau/Desktop/{database_name}/{database_name}:/var/lib/postgresql/data
-"""
-    pass
+# def create_postgress(database_name,database_user,database_password):
+#     compose = f"""
+# services:
+#   postgresql:
+#     image: postgres
+#     restart: always
+#     environment:
+#       POSTGRES_DB: {database_name}
+#       POSTGRES_USER: {database_user}
+#       POSTGRES_PASSWORD: {database_password}
+#     ports:
+#       - "5472:5432"
+#     volumes:
+#       - /home/arnau/Desktop/{database_name}/{database_name}:/var/lib/postgresql/data
+# """
+#     pass
