f87c5a3ab541bf576ec6302711a5ff5d7e364fc9
arnaualbert <arnaualbert2003@gmail.com>
2024-05-16 21:16:05 +0200

    storage

--- webapp/controllers/storage_controller.py
+++ webapp/controllers/storage_controller.py
@@ -1,20 +1,58 @@
-from flask import Blueprint, request, render_template, redirect,session, make_response
+from flask import Blueprint, request, render_template, redirect,session, make_response,send_file, abort
 from controllers.login_controller import check_is_logged
-from py_access_system.storage_UpDown import crear_user_espacio, crear_carpeta
+from py_access_system.storage_UpDown import crear_user_espacio, crear_carpeta, get_all_from_user
+import os
 
 storage_module = Blueprint('storage_module', __name__, template_folder='templates')
 
 
 @storage_module.route("/storage")
 def user_storage():
+    username = session.get("username")
     crear_user_espacio()
+    paths =get_all_from_user(username)
     if request.method == "GET" and check_is_logged():
-        return render_template("user_storage.html")
+        username = session.get("username")
+        paths = get_all_from_user(username)
+        return render_template("user_storage.html",paths=paths)
     else:
         return redirect("/login")
 
 
-    
+@storage_module.route('/download_file/<path:filename>')
+def download_file(filename):
+    try:
+        base_dir = f"/home/arnau/users_storage/pepe"
+        # Safely join the base directory and the requested file
+        file_path = os.path.join(base_dir, filename)
+        
+        # Ensure the file exists
+        if os.path.isfile(file_path):
+            return send_file(file_path, as_attachment=True)
+        else:
+            abort(404)  # File not found
+    except Exception as e:
+        abort(500)  # Internal server error
+
+
+@storage_module.route('/delete_file/<path:filename>')
+def delete_file(filename):
+    try:
+        base_dir = f"/home/arnau/users_storage/pepe"
+        # Safely join the base directory and the requested file
+        file_path = os.path.join(base_dir, filename)
+        
+        # Ensure the file exists
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            return redirect("/storage")
+        else:
+            abort(404)  # File not found
+    except Exception as e:
+        abort(500)  # Internal server error
+
+
+
 #########################################
 @storage_module.route("/new_folder", methods=["GET", "POST"])
 def user_new_folder():
--- webapp/py_access_system/storage_UpDown.py
+++ webapp/py_access_system/storage_UpDown.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, redirect, session
 import os
 from py_access_system.plantilla_docker_compose import create_docker_compose_template
 import re
+import json
 
 #comintario
 
@@ -15,6 +16,69 @@ def get_path_user(username):
     current_user = get_current_user()
     file_path = f"/home/{current_user}/users_storage/{username}"
     return file_path
+
+
+
+def extract_paths(data, current_path=""):
+    paths = {
+        "directories": [],
+        "files": []
+    }
+    for item in data:
+        if item["type"] == "directory":
+            dir_path = f"{current_path}/{item['name']}".replace("//", "/")
+            # paths["directories"].append(dir_path)
+            if "contents" in item:
+                sub_paths = extract_paths(item["contents"], dir_path)
+                paths["directories"].extend(sub_paths["directories"])
+                paths["files"].extend(sub_paths["files"])
+        elif item["type"] == "file":
+            file_path = f"{current_path}/{item['name']}".replace("//", "/")
+            paths["files"].append(file_path)
+    return paths
+
+# def extract_paths(data, current_path=""):
+#     paths = {}
+#     for item in data:
+#         if item["type"] == "directory":
+#             dir_path = f"{current_path}/{item['name']}".replace("//", "/")
+#             paths[dir_path] = []
+#             if "contents" in item:
+#                 sub_paths = extract_paths(item["contents"], dir_path)
+#                 paths.update(sub_paths)
+#         elif item["type"] == "file":
+#             file_path = f"{current_path}/{item['name']}".replace("//", "/")
+#             if current_path in paths:
+#                 paths[current_path].append(file_path)
+#             else:
+#                 paths[current_path] = [file_path]
+#     return paths
+
+
+# def extract_paths(data, current_path=""):
+#     paths = {}
+#     for item in data:
+#         if item["type"] == "directory":
+#             dir_path = f"{current_path}/{item['name']}".replace("//", "/")
+#             paths[dir_path] = {"files": [], "directories": {}}
+#             if "contents" in item:
+#                 sub_paths = extract_paths(item["contents"], dir_path)
+#                 paths[dir_path]["directories"].update(sub_paths)
+#         elif item["type"] == "file":
+#             file_path = f"{current_path}/{item['name']}".replace("//", "/")
+#             if current_path not in paths:
+#                 paths[current_path] = {"files": [], "directories": {}}
+#             paths[current_path]["files"].append(file_path)
+#     return paths
+
+
+def get_all_from_user(username):
+    path = get_path_user(username)
+    output = subprocess.check_output(["tree",path,"-J"])
+    paths = extract_paths(json.loads(output.decode()))
+    print(paths)
+    return paths
+
 
 #creamos un espacio para el usurio sino esta creado
 def crear_user_espacio():
